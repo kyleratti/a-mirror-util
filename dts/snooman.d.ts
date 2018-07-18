776c962bd6b9ef0009ced49dfddae3e26edb9093
Kyle <kyleratti@users.noreply.github.com>
2018-07-18 19:52:50 -0400

    Fixed brain fart so snooman works as intended now

--- dts/snooman.d.ts
+++ dts/snooman.d.ts
@@ -1,6 +1,8 @@
 import snoowrap from 'snoowrap';
-/**
- * Generates a snoowrap object
- * @returns A snoowrap object
- */
-export default function wrap(): snoowrap;
+export default class {
+    /**
+     * Generates a snoowrap object
+     * @returns A snoowrap object
+     */
+    static wrap(): snoowrap;
+}
--- src/snooman.ts
+++ src/snooman.ts
@@ -2,22 +2,24 @@ import snoowrap from 'snoowrap';
 
 import configurator from './configurator';
 
-/**
- * Generates a snoowrap object
- * @returns A snoowrap object
- */
-export default function wrap(): snoowrap {
-    let wrap = new snoowrap({
-        userAgent: configurator.reddit.userAgent,
-        clientId: configurator.reddit.clientID,
-        clientSecret: configurator.reddit.clientSecret,
-        username: configurator.reddit.username,
-        password: configurator.reddit.password,
-    });
+export default class {
+    /**
+     * Generates a snoowrap object
+     * @returns A snoowrap object
+     */
+    static wrap(): snoowrap {
+        let wrap = new snoowrap({
+            userAgent: configurator.reddit.userAgent,
+            clientId: configurator.reddit.clientID,
+            clientSecret: configurator.reddit.clientSecret,
+            username: configurator.reddit.username,
+            password: configurator.reddit.password,
+        });
 
-    wrap.config({
-        continueAfterRatelimitError: false // we will handle our own replies
-    });
-    
-    return wrap;
+        wrap.config({
+            continueAfterRatelimitError: false // we will handle our own replies
+        });
+        
+        return wrap;
+    }
 }
