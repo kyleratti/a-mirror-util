d3f19c2d33f3537570759b36d312de68663d7cfa
Kyle <kyleratti@users.noreply.github.com>
2019-11-09 14:33:36 -0500

    Updated response structure

--- src/structures/api/tuckbotprivateapi.ts
+++ src/structures/api/tuckbotprivateapi.ts
@@ -1,11 +1,30 @@
 import { MirroredVideo } from "..";
 
+export interface ApiResponse {
+  status: {
+    /** The HTTP status code of the response */
+    status: number;
+    /** The HTTP status code of the response */
+    statusCode?: number;
+    /** The custom message included with the response */
+    message: string;
+  };
+
+  /** Data returned from the response, if any */
+  data?: any;
+}
+
 export interface VideoUpdateRequestOptions {
   redditPostId: string;
   redditPostTitle: string;
   mirrorUrl: string;
 }
 
-export interface FetchStaleVideosResponse {
-  mirroredVideos: MirroredVideo[];
+export interface PruneVideoResponse extends ApiResponse {}
+
+export interface FetchStaleVideosResponse extends ApiResponse {
+  data: {
+    /** An array of mirrored videos that are stale */
+    mirroredVideos: MirroredVideo[];
+  };
 }
